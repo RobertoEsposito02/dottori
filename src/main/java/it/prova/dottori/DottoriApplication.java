293ade07b361198e10045d1f80b103cf8cc096fc
RobertoEsposito02 <robiespo02@libero.it>
2022-12-01 19:14:14 +0100

    creati new nell'application

--- src/main/java/it/prova/dottori/DottoriApplication.java
+++ src/main/java/it/prova/dottori/DottoriApplication.java
@@ -1,13 +1,36 @@
 package it.prova.dottori;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DottoriApplication {
+import it.prova.dottori.model.Dottore;
+import it.prova.dottori.service.DottoreService;
 
+@SpringBootApplication
+public class DottoriApplication implements CommandLineRunner{
+
+	@Autowired
+	private DottoreService dottoreService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DottoriApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		dottoreService.inserisciNuovo(Dottore.builder()
+				.nome("mario")
+				.cognome("rossi")
+				.codiceDottore("MarioRossi1")
+				.build());
+		
+		dottoreService.inserisciNuovo(Dottore.builder()
+				.nome("Luigi")
+				.cognome("Verdi")
+				.codiceDottore("LuigiVerdi1")
+				.build());
 	}
 
 }
