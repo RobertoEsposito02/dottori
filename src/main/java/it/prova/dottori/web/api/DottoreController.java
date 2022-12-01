6d8e16b625d4354f4c7ed9085d858e99b7ed983f
RobertoEsposito02 <robiespo02@libero.it>
2022-12-01 19:19:49 +0100

    aggiunto insert dottore

--- src/main/java/it/prova/dottori/web/api/DottoreController.java
+++ src/main/java/it/prova/dottori/web/api/DottoreController.java
@@ -3,15 +3,20 @@ package it.prova.dottori.web.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.dottori.dto.DottoreDTO;
 import it.prova.dottori.model.Dottore;
 import it.prova.dottori.service.DottoreService;
 import it.prova.dottori.web.api.exception.DottoreNotFoundException;
+import it.prova.dottori.web.api.exception.IdNotNullForInsertException;
 
 @RestController
 @RequestMapping("/api/dottore")
@@ -33,5 +38,16 @@ public class DottoreController {
 			throw new DottoreNotFoundException("nessun dottore trovato che stia visitando questo paziente");
 			
 		return DottoreDTO.buildDottoreDTOFromModel(result);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void inserisciDottore(@RequestBody DottoreDTO dottore) {
+		
+		if(dottore.getId() != null)
+			throw new IdNotNullForInsertException("impossibile inserire un nuovo record se contenente id");
+		
+		dottoreService.inserisciNuovo(dottore.buildDottoreModel());
+		
 	}
 }
