8aaf6bf9cd53b74336c85e90eab89316568253f6
RobertoEsposito02 <robiespo02@libero.it>
2022-12-01 17:24:02 +0100

    aggiunto service

---
+++ src/main/java/it/prova/dottori/service/DottoreServiceImpl.java
@@ -0,0 +1,47 @@
+package it.prova.dottori.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.prova.dottori.model.Dottore;
+import it.prova.dottori.repository.DottoreRepository;
+
+@Service
+public class DottoreServiceImpl implements DottoreService{
+
+	@Autowired
+	private DottoreRepository dottoreRepository;
+	
+	@Override
+	public List<Dottore> listAll() {
+		return (List<Dottore>) dottoreRepository.findAll();
+	}
+
+	@Override
+	public Dottore caricaSingoloElemento(Long id) {
+		return dottoreRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Dottore caricaSingoloElementoEager(Long id) {
+		return null;
+	}
+
+	@Override
+	public void aggiorna(Dottore dottoreInstance) {
+		dottoreRepository.save(dottoreInstance);
+	}
+
+	@Override
+	public void inserisciNuovo(Dottore dottoreInstance) {
+		dottoreRepository.save(dottoreInstance);
+	}
+
+	@Override
+	public void rimuovi(Long idToRemove) {
+		dottoreRepository.deleteById(idToRemove);
+	}
+
+}
